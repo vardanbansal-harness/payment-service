048ac12caf9aa827f9784c34144d091a7a2ecaef
nikpapag <nikolaos.papageorgiou@harness.io>
2021-11-16 20:40:07 +0000

    log commit

--- payment-service/src/main/java/com/nikp/cvdemo/logs/LoggingService.java
+++ payment-service/src/main/java/com/nikp/cvdemo/logs/LoggingService.java
@@ -4,8 +4,15 @@ public class LoggingService {
 	
 	
 	public void doLog() {
-		for (int i=0; i <10; i++) {
-			System.out.println("Testing Logs");	
+		for (int i=0; i <100; i++) {
+			System.out.println("app message - beatles test harness - sergeant pepper's lonely hearts club band");	
+			System.out.println("app message - beatles test harness - let it be");	
+			System.out.println("app message - beatles test harness -  here comes the sun");
+			System.out.println("app message - beatles test harness - with a little help from my friends");
+			System.out.println("app message - beatles test harness - Lucy in the sky with diamonds");
+			System.out.println("app message - beatles test harness - blackbird");
+			System.out.println("app message - beatles test harness - norwegian wood (this bird has flown)");
+			
 		}
 	}
 
